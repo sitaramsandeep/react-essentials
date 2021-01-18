81c507009453b4ed2775f9a0c91816b6835dce49
Sandeep Potnuru <spotnuru@zenhyd1.com>
2021-01-18 13:44:39 +0530

    adding component properties

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header() {
+function Header(props) {
+  console.log(props);
   return (
     <header>
-      <h1>Sandeep's Kitchen</h1>
+      <h1>{props.name}'s Kitchen</h1>
     </header>
   )
 }
 
-function Main() {
+function Main(props) {
   return (
     <section>
-      <p>We serve the most delicious food around</p>
+      <p>We serve the most {props.adjective} food around</p>
     </section>
   )
 }
 
-function Footer() {
+function Footer(props) {
   return (
     <section>
-      <p>It's true</p>
+      <p>Copyright {props.year}</p>
     </section>
   )
 }
@@ -28,9 +29,9 @@ function Footer() {
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header name="Deepu"/>
+      <Main adjective="amazing"/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
