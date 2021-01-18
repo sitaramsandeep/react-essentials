44920119699f4e055d9262fe4983440a4eeff111
Sandeep Potnuru <spotnuru@zenhyd1.com>
2021-01-18 11:54:07 +0530

    creating react components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <h1>Sandeep's Kitchen</h1>
+    </header>
+  )
+}
+
+function Main() {
+  return (
+    <section>
+      <p>We serve the most delicious food around</p>
+    </section>
+  )
+}
+
+function Footer() {
+  return (
+    <section>
+      <p>It's true</p>
+    </section>
+  )
+}
+
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
