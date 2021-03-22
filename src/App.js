60e0d8dc82212ba6c97a28ef04f6213b1ef785a1
Sandeep Potnuru <Sandeep.Potnuru@FISGLOBAL.COM>
2021-03-22 13:38:15 +0530

    displaying images with react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import restaurant from './restaurant.jpg'
 
 function Header(props) {
   return (
@@ -12,6 +13,9 @@ function Main(props) {
   return (
     <section>
       <p>We serve the most {props.adjective} food around</p>
+      <img src={restaurant} 
+        height={200} 
+        alt="napkin and silverware on table"/>
       <ul style={{textAlign:"left"}}>
         {props.dishes.map((dish) => (
         <li key={dish.id}>{dish.title}</li>
