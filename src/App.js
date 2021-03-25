1a5fb7debaf0d1e2aa0518de58200246dfba5f9d
Sandeep Potnuru <Sandeep.Potnuru@FISGLOBAL.COM>
2021-03-25 13:32:29 +0530

    nesting links with react router 6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { useEffect, useState, useReducer} from 'react';
 import './App.css';
-import restaurant from './restaurant.jpg'
 import {Route, Routes} from 'react-router-dom';
-import { Home, About, Events, ContactUs } from './pages'
+import { Home, About, Events, Contact, Whoops, Services, CompanyHistory, Location } from './pages'
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
+        <Route path="/about" element={<About/>}>
+          <Route path="services" element={<Services/>}/>
+          <Route path="companyHistory" element={<CompanyHistory/>}/>
+          <Route path="location" element={<Location/>}/>
+        </Route>
         <Route path="/events" element={<Events/>}/>
-        <Route path="/contactUs" element={<ContactUs/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/*" element={<Whoops/>}/>
       </Routes>
     </div>
   );
--- src/pages.js
+++ src/pages.js
@@ -1,7 +1,14 @@
+import { Link, Outlet, useLocation } from "react-router-dom";
+
 export function Home() {
   return(
     <div>
       <h1>[Company Website]</h1>
+      <nav>
+        <Link to="about">About</Link>
+        <Link to="events">Events</Link>
+        <Link to="contact">Contact</Link>
+      </nav>
     </div>
   )
 }
@@ -10,6 +17,31 @@ export function About() {
   return(
     <div>
       <h1>[About]</h1>
+      <Outlet/>
+    </div>
+  )
+}
+
+export function Services() {
+  return(
+    <div>
+      <h2>Our Services</h2>
+    </div>
+  )
+}
+
+export function CompanyHistory() {
+  return(
+    <div>
+      <h2>Our Company History</h2>
+    </div>
+  )
+}
+
+export function Location() {
+  return(
+    <div>
+      <h2>Our Location</h2>
     </div>
   )
 }
@@ -22,10 +54,19 @@ export function Events() {
   )
 }
 
-export function ContactUs() {
+export function Contact() {
   return(
     <div>
-      <h1>[ContactUs]</h1>
+      <h1>[Contact]</h1>
+    </div>
+  )
+}
+
+export function Whoops() {
+  let location = useLocation();
+  return(
+    <div>
+      <h1>Resource not found at {location.pathname}</h1>
     </div>
   )
 }
